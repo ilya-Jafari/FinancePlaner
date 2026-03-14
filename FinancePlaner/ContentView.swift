1cbd1efea1fa10c17b1bed68202672cf4d092f05
Ilya Jafari <ilya1382@gmail.com>
2026-03-14 14:43:05 +0100

    Initial Commit

---
+++ FinancePlaner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FinancePlaner
+//
+//  Created by ilya  on 14.03.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FinancePlaner/FinancePlanerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinancePlanerApp.swift
+//  FinancePlaner
+//
+//  Created by ilya  on 14.03.26.
+//
+
+import SwiftUI
+
+@main
+struct FinancePlanerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
